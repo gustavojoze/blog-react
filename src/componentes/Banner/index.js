f2b675efd5f9ec4f54dfaf796da1e8a53e437e79
gustavojoze <gustavojosesoaressantana@gmail.com>
2024-07-06 19:04:35 -0300

    O banner foi modificado com aprensentação e foto

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,6 +1,32 @@
 import styles from './Banner.module.css'
+import circuloColorido from 'assets/circulo_colorido.png'
+import minhaFoto from 'assets/minha_foto.png'
 
 export default function Banner(){
-    return "fsdfsfsdfs"
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, mundo
+                </h1>
+                <p className={styles.paragrafo}>
+                    Prazer, meu nome é Gustavo José. Bem vindo ao meu blog criado em React para aprender sobre o Hoock do React useLocation.
+                </p>
+            </div>
+            <div className={styles.imagens}>
+                <img
+                className={styles.circuloColorido}
+                src={circuloColorido}
+                aria-hidden={true}
+                />
+                <img
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt='Minha foto'
+                />
+                
+            </div>
+        </div>
+    )
     
 }
