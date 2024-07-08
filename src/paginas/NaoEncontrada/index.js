26eaed93d66a47b7d9358fc0cde066d1f7327ced
gustavojoze <gustavojosesoaressantana@gmail.com>
2024-07-07 23:17:34 -0300

    Implementação do Botão da pagina NaoEncontrada para retornar a pagina de inicio com o useNavigate

--- src/paginas/NaoEncontrada/index.js
+++ src/paginas/NaoEncontrada/index.js
@@ -1,7 +1,11 @@
+import BotaoPrincipal from "componentes/BotaoPrincipal"
 import styles from "./NaoEncontrada.module.css"
 import erro404 from "assets/erro_404.png"
+import { useNavigate } from "react-router-dom"
 
 export default function NaoEncontrado(){ 
+    const navegar = useNavigate()
+    
     return(
         <>
         <div className={styles.conteudoContainer}>
@@ -19,10 +23,14 @@ export default function NaoEncontrado(){
             <p className={styles.paragrafo}>
             Aguarde uns instantes e recarregue a página, ou volte para a página inicial. 
             </p>
-            <div className={styles.botaoContainer}>
-                <button>
+            <div 
+                 className={styles.botaoContainer}
+                 //-1 significa ir para uma pagina anterior
+                 onClick={()=>navegar("-1")}
+            >
+                <BotaoPrincipal tamanho="lg">
                     Voltar
-                </button>
+                </BotaoPrincipal>
             </div>
             <img 
                 className={styles.imagemCachorro}
